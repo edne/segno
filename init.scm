2cb69455a365b11997bbe504117af7c97edd32ba
Edoardo Negri <edne@gmx.com>
2017-08-10 21:55:53 +0200

    Syntactic sugar

--- init.scm
+++ init.scm
@@ -21,3 +21,6 @@
 (define (get-root-shape) (with-mutex mutex root-shape))
 
 (define (draw shape)     (with-mutex mutex (set! root-shape shape)))
+
+(define >_               draw)
+(define (@ . rest)       (apply list rest))
